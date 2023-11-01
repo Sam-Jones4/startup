c736e88315f5d6b6032d26dd9e8aaabfc650341c
Sam Jones <sam.jones4844@gmail.com>
2023-10-31 23:02:00 -0600

    Fixed login.js for my code

--- login.js
+++ login.js
@@ -1,5 +1,7 @@
 function login() {
-    const nameEl = document.querySelector("#name");
-    localStorage.setItem("userName", nameEl.value);
-    window.location.href = "play.html";
+    const userName = document.querySelector("#username");
+    localStorage.setItem("userName", userName.value);
+    const passWord = document.querySelector("#password");
+    localStorage.setItem("passWord", passWord.value);
+    window.location.href = "chat.html";
 }
