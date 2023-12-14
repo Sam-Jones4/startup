ff0798f1f010fa5f5f3ad53e2edea7820ae20dc9
Sam Jones <sam.jones4844@gmail.com>
2023-12-13 17:20:22 -0700

    Fixed storage and sending of chats

--- public/chat.js
+++ public/chat.js
@@ -2,6 +2,36 @@ const messageInput = document.getElementById('send-message');
 const sendButton = document.querySelector('.send-message-bar svg');
 const chatContainer = document.querySelector('.active-chat');
 const userName = getUserName();
+const allMessages = document.getElementById('msgs')
+
+async function loadMessages() {
+    let messages = [];
+    try {
+      // Get the latest high scores from the service
+      const response = await fetch('/api/chat');
+      messages = await response.json();
+  
+      // Save the scores in case we go offline in the future
+      localStorage.setItem('messages', JSON.stringify(messages));
+    } catch {
+      // If there was an error then just use the last saved scores
+      const messagesText = localStorage.getItem('messages');
+      if (messagesText) {
+        messages = JSON.parse(messagesText);
+      }
+    }
+  
+    displayMessages(messages);
+}
+
+function displayMessages(messages)
+{
+    for (let int = 0; int < messages.length; int++) 
+    {
+        let message = messages[int];
+        addMessageToChat(message);
+    }
+}
 
 sendButton.addEventListener('click', () => {
     const message = messageInput.value;
@@ -9,18 +39,11 @@ sendButton.addEventListener('click', () => {
     messageInput.value = '';
 });
 
-function addSentMessageToChat(message) {
-    const sentMessageElement = document.createElement('div');
-    sentMessageElement.className = 'sent-chat active-chat';
-    sentMessageElement.textContent = message + " - " + userName;
-    chatContainer.appendChild(sentMessageElement);
-}
-
-function addReceivedMessageToChat(message) {
-    const receivedMessageElement = document.createElement('div');
-    receivedMessageElement.className = 'received-chat active-chat';
-    receivedMessageElement.textContent = message;
-    chatContainer.appendChild(receivedMessageElement);
+function addMessageToChat(message) {
+    const messageContent = document.createElement('div');
+    messageContent.className = 'sent-chat active-chat';
+    messageContent.textContent = message + " - " + userName;
+    chatContainer.appendChild(messageContent);
 }
 
 function getUserName() {
